a8e0986269ea6eda9a6198ee35801932421bec28
PZDuck <agp2281337@gmail.com>
2020-07-03 23:21:44 -0400

    8-ball exercise

---
+++ react-exercises/react-props/eightball/src/EightBall.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import answers from "./answers";
+import "./style.css";
+
+function EightBall() {
+  function getRandom() {
+    const answer = answers[Math.floor(Math.random() * answers.length)];
+    setAnswer(answer.msg);
+    setColor(answer.color);
+  }
+  function reset() {
+    setAnswer("Think of a Question");
+    setColor("black");
+    setCounter({
+      questions: 0,
+      green: 0,
+      goldenrod: 0,
+      red: 0,
+    });
+  }
+  function updateCounter() {
+    counter["questions"] += 1;
+    counter[color] += 1;
+    setCounter(counter);
+  }
+
+  const [answer, setAnswer] = useState("Think of a Question");
+  const [color, setColor] = useState("black");
+  const [counter, setCounter] = useState({
+    questions: 0,
+    green: 0,
+    goldenrod: 0,
+    red: 0,
+  });
+
+  return (
+    <>
+      <div
+        className="eightBall"
+        style={{ backgroundColor: color }}
+        onClick={() => {
+          getRandom();
+          updateCounter();
+        }}
+      >
+        <div className="eightBall-text">{answer}</div>
+      </div>
+
+      <button onClick={() => reset()}>Reset</button>
+      <span>{`#Questions: ${counter.questions} | #Positive: ${counter.green} | #Negative: ${counter.red} | #Neutral: ${counter.goldenrod}`}</span>
+    </>
+  );
+}
+
+export default EightBall;
